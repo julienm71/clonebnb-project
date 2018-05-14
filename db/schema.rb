41da92bb4b828b40dc34dc0befce2e796a7c80e7
Florian HAMEL <f.hamel64@gmail.com>
2018-05-14 18:57:31 +0200

    seed-faker-change column address

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180514144222) do
+ActiveRecord::Schema.define(version: 20180514164637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "places", force: :cascade do |t|
-    t.string "adress"
     t.string "photo"
     t.string "name"
     t.text "description"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
     t.index ["user_id"], name: "index_places_on_user_id"
   end
 
