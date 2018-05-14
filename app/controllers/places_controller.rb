0bab18dd601ae93ca4e7a6409868fec2d083024a
Julien Martin <julienm.contact@gmail.com>
2018-05-14 18:12:21 +0200

    no login index places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,16 @@
 class PlacesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
+  def index
+    @places = Place.all
+  end
+
+  def show
+    @place = Place.find(params[:id])
+  end
+
+private
+
+  def place_params
+    params.require(:place).permit(:description, :name, :photo, :address)
+  end
 end
