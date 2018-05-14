1415507f0b306d886a9ec81ce3624d0214816cc8
Florian HAMEL <f.hamel64@gmail.com>
2018-05-14 16:47:58 +0200

    change description to text

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180514142340) do
+ActiveRecord::Schema.define(version: 20180514144222) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20180514142340) do
     t.string "adress"
     t.string "photo"
     t.string "name"
-    t.string "description"
+    t.text "description"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
