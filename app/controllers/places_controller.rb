19b7637806d9905534133ff2c9b1c87c3f7a3b1f
mateo electorat <electoratmateo@free.fr>
2018-05-14 17:35:31 +0200

    added show method and place_params private method

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,13 @@
 class PlacesController < ApplicationController
+
+  def show
+  @place = Place.find(params[:id])
+  end
+
+private
+
+  def place_params
+    params.require(:place).permit(:description, :name, :photo, :address)
+  end
+
 end
