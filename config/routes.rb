f9c6cbaac2fab2e9d292130601bea3ddae282beb
Julien Martin <julienm.contact@gmail.com>
2018-05-15 15:15:14 +0200

    fix routes + gmaps fix for flo

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  resources :users
-
   resources :places
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users, controllers: {
         sessions: 'users/sessions'
   }
