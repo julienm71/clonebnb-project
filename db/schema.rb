075715375859830b726bf1f39afd61e843dc7c7b
Florian HAMEL <f.hamel64@gmail.com>
2018-05-16 18:22:43 +0200

    Prepare for Heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180516144116) do
+ActiveRecord::Schema.define(version: 20180516152104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,9 +29,9 @@ ActiveRecord::Schema.define(version: 20180516144116) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "address"
-    t.integer "price"
     t.float "latitude"
     t.float "longitude"
+    t.integer "price"
     t.index ["user_id"], name: "index_places_on_user_id"
   end
 
