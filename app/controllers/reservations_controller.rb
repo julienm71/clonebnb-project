5b30c926d9f7cbe0edd56c8ba91d84671447eb4e
mateo electorat <electoratmateo@free.fr>
2018-05-16 13:40:45 +0200

    added devise authentication policy to ReservationsController

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,3 +1,16 @@
 class ReservationsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_place_instance
 
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def destroy
+
+  end
 end
