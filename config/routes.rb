53f4199fb8ba812f70f146de89a3d85afc0294c0
Julien Martin <julienm.contact@gmail.com>
2018-05-17 17:35:21 +0200

    about page, home, gem float

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   root to: 'pages#home'
+  get "pages/about", to: "pages#about", as: "about"
   resources :places
   resources :reservations, only: [:index, :create, :new, :destroy]
   devise_for :users
