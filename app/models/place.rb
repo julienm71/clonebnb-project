14a7441f36e3d931a89e766367ac08f740196dd5
Julien Martin <julienm.contact@gmail.com>
2018-05-18 07:54:48 +0200

    review base

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,7 @@
 class Place < ApplicationRecord
   belongs_to :user
   has_many :reservations
+  has_many :reviews, dependent: :destroy
   mount_uploader :photo, PhotoUploader
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
